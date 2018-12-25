dacef5eeb0eac5b7fe8c9ef7998e82faae9c529a
persistInHacker <874387886@qq.com>
2018-12-25 22:53:40 +0800

    命令模式

---
+++ src/designModel/command/OffCommand.java
@@ -0,0 +1,17 @@
+package designModel.command;
+/**
+ * 具体命令角色 
+ * 包含一个接收者角色对象的引用
+ *
+ */
+public class OffCommand implements Command {
+	private TV tv;
+	public OffCommand(TV tv){
+		this.tv = tv;
+	}
+	@Override
+	public void execute() {
+		tv.turnOff();
+	}
+
+}
